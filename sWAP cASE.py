5be1a7360cf7d707396f5de86f18406b808f7ebf
Rashidul Hasan Hridoy <63078167+rashidulhasanhridoy@users.noreply.github.com>
2020-07-26 02:33:35 +0600

    Create sWAP cASE.py

---
+++ sWAP cASE.py
@@ -0,0 +1,15 @@
+def swap_case(s):
+    s1 = ''
+    for i in s:
+        if i.isupper():
+            s1 = s1 + i.lower()
+        elif i.islower():
+            s1 = s1 + i.upper()
+        else:
+            s1 = s1 + i
+    return s1               
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
